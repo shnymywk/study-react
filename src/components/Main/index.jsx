7e3c8785e3dbb56759b11082c65adb7876c427da
shnymywk <shnymywk@gmail.com>
2026-05-03 09:10:39 +0900

    ✨ useEffectの追加テスト

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,25 +1,23 @@
 import Image from "next/image";
-import { Geist, Geist_Mono } from "next/font/google";
 import { Links } from "@/components/Links/index";
 import { Headline } from "@/components/Headline/index";
 import styles from "@/components/Main/index";
+import { useEffect } from "react";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export function Main(props) {
+  useEffect(() => {
+      document.body.style.backgroundColor = "lightblue";
+      return (() => {
+        document.body.style.backgroundColor = "white";
+      });
+    },[]);
+
   return (
     <div
-      className={`${geistSans.className} ${geistMono.className} flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black`}
+      className={` flex min-h-screen items-center justify-center font-sans `}
     >
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 sm:items-start">
         <Image
           className="dark:invert"
           src="/next.svg"
@@ -27,9 +25,10 @@ export function Main(props) {
           width={100}
           height={20}
           priority
+          style={{ height: "auto" }}
         />
         <Headline title={props.title}>
-            <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">To get started, edit the {props.title} file.</h1>
+            <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight ">To get started, edit the {props.title} file.</h1>
         </Headline>
         <Links />
       </main>
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import { Main } from "@/components/Main";
 import { Header } from "@/components/Header";
-import { useCallback } from "react";
+import { useCallback, useEffect } from "react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -20,22 +20,29 @@ const geistMono = Geist_Mono({
 // };
 
 export default function Home() {
-  const foo = 1;
+//   const foo = 1;
   
-  const handleClick = useCallback ((e) => {
-  console.log(e.target.href);
-  e.preventDefault();
-  alert(foo);
-},[]); 
+//   const handleClick = useCallback ((e) => {
+//   console.log(e.target.href);
+//   e.preventDefault();
+//   alert(foo);
+// },[]); 
+
+  // useEffect(() => {
+  //   document.body.style.backgroundColor = "lightblue";
+  //   return (() => {
+  //     document.body.style.backgroundColor = "";
+  //   });
+  // },[]);
 
   return (
     <>
     <Header />
-    <a
+    {/* <a
       href="/about"
       onClick={handleClick}>
       ボタン
-    </a>
+    </a> */}
     <Main title="Index"/>
     </>
   );
