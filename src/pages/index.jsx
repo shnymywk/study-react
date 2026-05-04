90eb056cf849fa7187272c61b25bf12e9b6cf119
shnymywk <shnymywk@gmail.com>
2026-05-04 18:03:40 +0900

    useStateでinputの追加とbooleanによる表示：非表示設定

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -15,6 +15,8 @@ const geistMono = Geist_Mono({
 
 export default function Home() {
   const [count,setCount] = useState(1)
+  const [text,setText] = useState("");
+  const [isShow,setIsShow] = useState(true);
   
   const handleClick = useCallback(() => {
     if (count < 10){
@@ -22,18 +24,40 @@ export default function Home() {
     }
   },[count]); 
 
+  const handleDisplay = useCallback(() => {
+    setIsShow((isShow) => !isShow);
+  },[])
+
   useEffect(() => {
     document.body.style.backgroundColor = "lightblue";
-    return (() => {
-      document.body.style.backgroundColor = "";
+      return (() => {
+    document.body.style.backgroundColor = "";
     });
+  },[]);
+
+  const handleChange = useCallback((e) => {
+    if (e.target.value.length > 5){
+      alert("5文字以内にしてください")
+      return;
+    }
+    setText(e.target.value.trim());
   },[]);
 
   return (
     <>
     <Header />
-    <h1>{count}</h1>
+    {isShow ? <h1>{count}</h1> : null}
     <button onClick={handleClick}>ボタン</button>
+    <button 
+      onClick={handleDisplay}
+    >
+    {isShow ? "非表示" : "表示"}
+    </button>
+    <input 
+      type="text" 
+      value={text} 
+      onChange={handleChange}
+    />
     <Main title="Index" bgColor="lightblue"/>
     </>
   );
