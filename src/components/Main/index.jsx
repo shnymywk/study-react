e8eb5d00a35dd71f16be878086e8ba9a48855101
shnymywk <shnymywk@gmail.com>
2026-05-06 03:53:29 +0900

    🎨 Custom Hooksの実装

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,8 +1,9 @@
 import Image from "next/image";
 import { Links } from "@/components/Links/index";
 import { Headline } from "@/components/Headline/index";
-import styles from "@/components/Main/index";
 import { useEffect } from "react";
+import { useCounter } from "@/components/hooks/useCounter";
+import { useInputArray } from "@/components/hooks/useInputArray";
 
 
 export function Main(props) {
@@ -14,11 +15,28 @@ export function Main(props) {
       };
     },[]);
 
+  const {count,isShow,handleClick,handleDisplay} = useCounter();
+  const {text,array,handleChange,handleAdd} = useInputArray();
+
+
   return (
     <div
       className={` flex min-h-screen items-center justify-center font-sans `}
     >
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 sm:items-start">
+        
+        {isShow ? <h1>{count}</h1> : null}
+        <button onClick={handleClick}>ボタン</button>
+        <button onClick={handleDisplay}>{isShow ? "非表示" : "表示"}</button>
+
+        <input  type="text" value={text} onChange={handleChange}/>
+        <button onClick={handleAdd}>追加</button>
+        <ul>
+          {array.map((item) => {
+            return <li key={item}>{item}</li>;
+          })}
+        </ul>
+        
         <Image
           className="dark:invert"
           src="/next.svg"
@@ -28,9 +46,11 @@ export function Main(props) {
           priority
           style={{ height: "auto" }}
         />
+
         <Headline title={props.title}>
             <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight ">To get started, edit the {props.title} file.</h1>
         </Headline>
+
         <Links />
       </main>
     </div>
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,66 +1,10 @@
-import { Geist, Geist_Mono } from "next/font/google";
 import { Main } from "@/components/Main";
 import { Header } from "@/components/Header";
-import { useCallback, useEffect, useState } from "react";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export default function Home() {
-  const [count,setCount] = useState(1)
-  const [text,setText] = useState("");
-  const [isShow,setIsShow] = useState(true);
-  const [array,setArray] = useState([]);
-  
-  const handleClick = useCallback(() => {
-    if (count < 10){
-    setCount((prevCount) => prevCount + 1);
-    }
-  },[count]); 
-
-  const handleDisplay = useCallback(() => {
-    setIsShow((prevIsShow) => !prevIsShow);
-  },[])
-
-  const handleChange = useCallback((e) => {
-    if (e.target.value.length > 5){
-      alert("5文字以内にしてください")
-      return;
-    }
-    setText(e.target.value.trim());
-  },[]);
-
-  const handleAdd = useCallback(() => {
-    setArray((prevArray) => {
-      if (prevArray.some(item => item === text)){
-        alert("同じ要素が既に存在します。")
-        return prevArray;
-      }
-      return [...prevArray, text];
-    });
-  },[text])
-
-
   return (
     <>
     <Header />
-    {isShow ? <h1>{count}</h1> : null}
-    <button onClick={handleClick}>ボタン</button>
-    <button onClick={handleDisplay}>{isShow ? "非表示" : "表示"}</button>
-    <input  type="text" value={text} onChange={handleChange}/>
-    <button onClick={handleAdd}>追加</button>
-    <ul>
-      {array.map((item) => {
-        return <li key={item}>{item}</li>;
-      })}
-    </ul>
     <Main title="Index" bgColor="lightblue"/>
     </>
   );
